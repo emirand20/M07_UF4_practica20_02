65748854dde950228c23d570f7da30c203764df6
4Tipy <lchavez21@jaumebalmes.net>
2023-05-11 20:40:03 +0200

    cambios en la view y en settings

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
+import statistics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.generics import get_object_or_404
 from .models import Producto
 from .serializers import ProductoSerializer
 
@@ -12,11 +13,7 @@ def lista_productos(request):
 
 @api_view(['GET'])
 def detalle_producto(request, pk):
-    try:
-        producto = Producto.objects.get(pk=pk)
-    except Producto.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
+    producto = get_object_or_404(Producto, pk=pk)
     serializer = ProductoSerializer(producto)
     return Response(serializer.data)
 
@@ -25,28 +22,26 @@ def agregar_producto(request):
     serializer = ProductoSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
-@api_view(['PUT'])
+@api_view(['GET', 'PUT', 'PATCH'])
 def actualizar_producto(request, pk):
-    try:
-        producto = Producto.objects.get(pk=pk)
-    except Producto.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    producto = get_object_or_404(Producto, pk=pk)
+
+    if request.method == 'GET':
+        serializer = ProductoSerializer(producto)
+        return Response(serializer.data)
 
     serializer = ProductoSerializer(producto, data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=statistics.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['DELETE'])
 def eliminar_producto(request, pk):
-    try:
-        producto = Producto.objects.get(pk=pk)
-    except Producto.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
+    producto = get_object_or_404(Producto, pk=pk)
     producto.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response(status=204)
