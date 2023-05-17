e524a4e980784b5ff405f00dcd79971018ad16c9
Asus <emirand20>
2023-05-17 21:00:46 +0200

    carreto

--- carreto/models.py
+++ carreto/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from catalog.models import Producto
 
-# Create your models here.
 class Carreto(models.Model):
-    id = models.AutoField(auto_created=True, primary_key=True, serialize=False)
-    nombreCarreto =  models.CharField(max_length=50,default=None)
     productos = models.ManyToManyField(Producto)
     isBuy = models.BooleanField(default=False)
+    #suario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    
+    def __str__(self):
+        return f"Carreto {self.id}"
